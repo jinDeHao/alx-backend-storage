66ca4626acdadd897609311bae00ebfccfead20b
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 01:41:33 +0100

    mongo_collection.find()

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""List all documents in Python """
+import pymongo
+
+
+def list_all(mongo_collection) -> list:
+    """List all documents in Python """
+    return mongo_collection.find()
