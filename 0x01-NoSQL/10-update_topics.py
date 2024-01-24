83de0888bf8d858c62dc77387185e589c2a50928
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 01:59:10 +0100

    Where can I learn Python?

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""update topics where the name == name"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """update topics where the name == name"""
+    return mongo_collection.update_many(
+        {"name": name}, {"$set": {"topics": topics}},
+    )
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""update topics where the name == name"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """update topics where the name == name"""
+    return mongo_collection.find({"topic": topic})
