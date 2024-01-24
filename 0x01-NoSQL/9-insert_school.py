2ba80527b04cf2ac2a2afc3fe72040c8cf09ca76
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 01:45:53 +0100

    inserted_id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""List all documents in Python """
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """List all documents in Python """
+    return mongo_collection.insert_one(kwargs).inserted_id
