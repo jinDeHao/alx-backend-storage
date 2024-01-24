08240c785130eb0c132a6439fe902292cdcc1ea7
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 02:40:43 +0100

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
+"""Python script that provides some stats
+about Nginx logs stored in MongoDB"""
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["logs"]
-mycol = mydb["nginx"]
+if __name__ == "__main__":
+    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    mydb = myclient.logs
+    mycol = mydb.nginx
 
-print("{} logs".format(len(mycol.find())))
-print("Methods:".format(len(mycol.find())))
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(len(mycol.find())))
+    print("Methods:".format(len(mycol.find())))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+    for m in methods:
+        print("\tmethod {}: {}".format(m, len(mycol.find({"method": m}))))
 
-for m in methods:
-    print("\tmethod {}: {}".format(m, len(mycol.find({"method": m}))))
-
-print("{} status check".format(
-    len(mycol.find({"method": "GET", "path": "/status"}))))
+    print("{} status check".format(
+        len(mycol.find({"method": "GET", "path": "/status"}))))
