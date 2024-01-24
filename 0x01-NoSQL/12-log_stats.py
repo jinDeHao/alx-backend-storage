d35d6a5344d5adf16425460810754c1c6c765b81
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 02:33:18 +0100

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["logs"]
+mycol = mydb["nginx"]
+
+print("{} logs".format(len(mycol.find())))
+print("Methods:".format(len(mycol.find())))
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+for m in methods:
+    print("\tmethod {}: {}".format(m, len(mycol.find({"method": m}))))
+
+print("{} status check".format(
+    len(mycol.find({"method": "GET", "path": "/status"}))))
