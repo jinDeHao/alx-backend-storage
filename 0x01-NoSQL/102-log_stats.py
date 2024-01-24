49a9f18a56ffd8bb57c3dfe7ebcb89e28d5d64cb
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 03:13:13 +0100

    102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,38 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
+
+import pymongo
+from collections import Counter
+
+if __name__ == "__main__":
+    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    mydb = myclient.logs
+    mycol = mydb.nginx
+
+    print("{} logs".format(mycol.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for m in methods:
+        print("\tmethod {}: {}".format(m, mycol.count_documents({"method": m})))
+
+    print("{} status check".format(
+        mycol.count_documents({"method": "GET", "path": "/status"})))
+    print("IPs:")
+
+    mydocs = mycol.find()
+    counting_dict = sorted(
+        Counter(one['ip'] for one in mydocs).items(),
+        key=lambda one: one[1],
+        reverse=True
+    )
+    i = 1
+
+    for ip in counting_dict:
+        print(f"\t{ip}: {counting_dict[ip]}")
+        if i == 10:
+            break
+        i += 1
