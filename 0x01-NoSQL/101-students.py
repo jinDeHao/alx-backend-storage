4441485dc4c1025aefcdf822675e6733a2c7f3ef
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 03:52:52 +0100

    done i think

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,10 +6,13 @@ import pymongo
 def top_students(mongo_collection):
     """averageScore"""
     stus = mongo_collection.find()
-    average = {}
+    sorted_stud = []
     for s in stus:
         sum = 0
+        average = {}
+        average = s
         for t in s["topics"]:
             sum += t["score"]
-        average[s["name"]] = sum / len(s["topics"])
-    return average
+        average["averageScore"] = sum / len(s["topics"])
+        sorted_stud.append(average)
+    return sorted(sorted_stud, key=lambda x: x["averageScore"])
