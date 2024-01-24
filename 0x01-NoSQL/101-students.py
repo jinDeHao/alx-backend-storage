6553c7120368ba3e5e81463ee210c051fee4911b
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-24 03:36:30 +0100

    101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""List all documents in Python """
+import pymongo
+
+
+def top_students(mongo_collection):
+    """averageScore"""
+    return mongo_collection.find().sort("averageScore")
